643afc302f9084361e0c04bf4fc92d7ac9c7b933
Alejandro Lopez Presa <a.lopez.presa@gmail.com>
2023-04-14 12:15:40 -0300

    2020: removed dumb things from day1.py

--- 2020/day1.py
+++ 2020/day1.py
@@ -12,19 +12,17 @@ with open('input-day1.txt') as f:
             y = int(y)
 
             if not done1 and x + y == 2020:
-                print(("part 1", x * y))
+                print("part 1", x * y)
                 done1 = True
 
             if not done2:
                 for z in numbers:
                     z = int(z)
                     if x + y + z == 2020:
-                        print(("part 2", x * y * z))
+                        print("part 2", x * y * z)
                         done2 = True
                         break
 
             if done1 and done2:
-                break
-        if done1 and done2:
-            break
+                exit(0)
 
