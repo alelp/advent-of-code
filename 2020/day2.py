0f7cd7f4daa192e697ff6f11b704a38438a7edeb
Alejandro Lopez Presa <a.lopez.presa@gmail.com>
2023-04-17 11:00:05 -0300

    2020: 'optimized' day by not making the same thing multiple times

--- 2020/day2.py
+++ 2020/day2.py
@@ -3,23 +3,20 @@
 with open('input-day2.txt') as f:
     passwords = f.read().splitlines()
 
-    valid = 0
+    check1_valids = 0
+    check2_valids = 0
     for password_data in passwords:
         limits, letter, password = [x.strip(' :') for x in password_data.split(' ')]
         limit_min, limit_max = [int(x) for x in limits.split('-')]
-        count = password.count(letter);
+        count = password.count(letter)
+
         if count >= limit_min and count <= limit_max:
-            valid += 1
-    print('part 1:', valid)
+            check1_valids += 1
 
-    valid = 0
-    for password_data in passwords:
-        limits, letter, password = [x.strip(' :') for x in password_data.split(' ')]
-        pos_1, pos_2 = [int(x) - 1 for x in limits.split('-')]
-        if password[pos_1] == letter and password[pos_2] != letter:
-            valid += 1
-        elif password[pos_1] != letter and password[pos_2] == letter:
-            valid += 1
-    print('part 2:', valid)
+        pos_1, pos_2 = limit_min - 1, limit_max - 1
+        if (password[pos_1] == letter and password[pos_2] != letter) or (password[pos_1] != letter and password[pos_2] == letter):
+            check2_valids += 1
 
+    print('part 1:', check1_valids)
+    print('part 2:', check2_valids)
 
