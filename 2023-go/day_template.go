e76bcbc7668a8c06af9a32436312169cfbc968de
Alejandro Lopez Presa <a.lopez.presa@gmail.com>
2023-12-04 20:15:09 -0300

    added template file for new days

---
+++ 2023-go/day_template.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+ _ "fmt"
+ _ "strconv"
+ _ "strings"
+)
+
+func main() {
+  part1 := DayPart{
+    []string {
+    },
+    -1,
+    part1,
+  }
+
+  part2 := DayPart{
+    []string {
+    },
+    -1,
+    part2,
+  }
+
+  day := 0
+  RunTests(day, part1, part2);
+  RunDay(day, part1, part2);
+}
+
+func part1(lines []string) int {
+  var result = -1
+  return result
+}
+
+func part2(lines []string) int {
+  var result = -1
+  return result
+}
