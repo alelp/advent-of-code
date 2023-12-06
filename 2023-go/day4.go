18288fe3267a53a642dab72e2ac83a27da79456e
Alejandro Lopez Presa <a.lopez.presa@gmail.com>
2023-12-06 18:03:11 -0300

    2023-go day4 part 2

--- 2023-go/day4.go
+++ 2023-go/day4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -36,8 +37,57 @@ func part1(lines []string) int {
   return result
 }
 
+
 func part2(lines []string) int {
-  var result = -1
+  result := 0
+
+  cards_db := make(map[int]int)
+
+  for _, line := range lines {
+    card_parts := strings.Split(line, ":")
+    tmp := strings.ReplaceAll(card_parts[0], "Card", "")
+    tmp = strings.ReplaceAll(tmp, " ", "")
+    card_id, _ := strconv.Atoi(tmp)
+    numbers := strings.Split(card_parts[1], "|")
+    winning_numbers := strings.Split(numbers[0], " ")
+    card_numbers := strings.Split(numbers[1], " ")
+
+    var card_score = 0
+
+    for _, num := range card_numbers {
+      for _, win_num := range winning_numbers {
+        if num == win_num && num != "" {
+          card_score++
+          break;
+        }
+      }
+    }
+    cards_db[card_id] = card_score
+  }
+
+
+  var get_won_cards func(card_id int) int
+  get_won_cards = func(card_id int) int {
+    earned_cards := 0
+
+    score, exists := cards_db[card_id]
+
+    if exists {
+      earned_cards++
+
+      for score > 0 {
+        earned_cards += get_won_cards(card_id + score)
+        score--
+      }
+    }
+
+    return earned_cards
+  }
+
+  for id := range cards_db {
+    result += get_won_cards(id)
+  }
+
   return result
 }
 
